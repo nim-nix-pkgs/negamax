f988a6cda3c0d09dfa7244eb729f3fc2cb7c6c15
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:18 -0300

    chore: re index negamax

--- flake.nix
+++ flake.nix
@@ -7,6 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."negamax-master".dir   = "master";
+  inputs."negamax-master".owner = "nim-nix-pkgs";
+  inputs."negamax-master".ref   = "master";
+  inputs."negamax-master".repo  = "negamax";
+  inputs."negamax-master".type  = "github";
+  inputs."negamax-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."negamax-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."negamax-v0_0_1".dir   = "v0_0_1";
+  inputs."negamax-v0_0_1".owner = "nim-nix-pkgs";
+  inputs."negamax-v0_0_1".ref   = "master";
+  inputs."negamax-v0_0_1".repo  = "negamax";
+  inputs."negamax-v0_0_1".type  = "github";
+  inputs."negamax-v0_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."negamax-v0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."negamax-v0_0_2".dir   = "v0_0_2";
+  inputs."negamax-v0_0_2".owner = "nim-nix-pkgs";
+  inputs."negamax-v0_0_2".ref   = "master";
+  inputs."negamax-v0_0_2".repo  = "negamax";
+  inputs."negamax-v0_0_2".type  = "github";
+  inputs."negamax-v0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."negamax-v0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."negamax-v0_0_3".dir   = "v0_0_3";
+  inputs."negamax-v0_0_3".owner = "nim-nix-pkgs";
+  inputs."negamax-v0_0_3".ref   = "master";
+  inputs."negamax-v0_0_3".repo  = "negamax";
+  inputs."negamax-v0_0_3".type  = "github";
+  inputs."negamax-v0_0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."negamax-v0_0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
